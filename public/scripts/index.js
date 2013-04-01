4ca60a7316ae526f9da075e0669ef03e1ccc3cff
Ryan <retric@gmail.com>
2013-04-01 14:17:53 -0700

    index.js first impl

---
+++ public/scripts/index.js
@@ -0,0 +1,34 @@
+$(function() {
+
+  $('#friendlist').autocomplete({
+    source: function (req, res) {
+     $.ajax( {
+        type: "POST",
+        url: "/friendlist",
+        dataType: "json",
+        data: {
+          name_startsWith: req.term,
+          uid: '<%= user.id %>'
+        },
+        success: function(data) {
+          res($.map(data, function (friend) {
+            return {
+              label: friend.name,
+              id: friend.id
+            }
+          }).slice(0, 10)); 
+        },
+        error: function() {
+          console.log("ajax error");
+        }
+      });
+    },
+    minLength: 1,
+    select: function(event, ui) {
+    $('#friendlist').val(ui.item.label);
+    var link = "/" + '<%= user.username %>' + "/" + ui.item.id;
+    window.location.href = link;
+    } 
+  });
+
+});
